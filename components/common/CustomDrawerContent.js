119683e26255aa2f82042974fe97b45dd94fc9fa
SamrawitMM <samrawitmulu88@gmail.com>
2024-05-18 12:17:28 +0300

    before adding the picker to headere

---
+++ components/common/CustomDrawerContent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View style={styles.drawerHeader}>
+        <Text style={styles.drawerHeaderText}>Custom Header</Text>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  drawerHeader: {
+    height: 150,
+    backgroundColor: '#f4f4f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  drawerHeaderText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomDrawerContent;
--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { Drawer } from 'expo-router/drawer';
 import HomeScreen from '../components/screens/HomeScreen';
 import ProfileScreen from '../components/screens/ProfileScreen';
 import SettingsScreen from '../components/screens/SettingsScreen';
 import CustomDrawerHeader from './CustomDrawerHeader';
+import LanguagePicker from '../components/common/LanguagePicker';
 
 const Drawer = createDrawerNavigator();
-
-// const DrawerNavigator = () => (
-//   <Drawer.Navigator initialRouteName="Home">
-//     <Drawer.Screen name="Home" component={HomeScreen} />
-//     <Drawer.Screen name="Profile" component={ProfileScreen} />
-//     <Drawer.Screen name="Settings" component={SettingsScreen} />
-//   </Drawer.Navigator>
-// );
 
 const CustomDrawerContent = (props) => (
   <DrawerContentScrollView {...props}>
@@ -23,9 +17,15 @@ const CustomDrawerContent = (props) => (
 );
 
 const DrawerNavigator = () => (
+
   <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-    <Drawer.Screen name="Home" component={HomeScreen} />
-    <Drawer.Screen name="Profile" component={ProfileScreen} />
+  <Drawer.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{
+          headerRight: () => <LanguagePicker />
+        }}
+      />    <Drawer.Screen name="Profile" component={ProfileScreen} />
     <Drawer.Screen name="Settings" component={SettingsScreen} />
   </Drawer.Navigator>
 );
